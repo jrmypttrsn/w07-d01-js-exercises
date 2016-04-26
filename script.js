92e0c0189c2e0f5f3a14632c1183ac23ac08a21d
Jeremy Patterson <jrmypttrsn@gmail.com>
2016-04-25 19:27:28 -0500

    Added even or odd script

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ function shoutyHello() {
 	var name = prompt("What is your name?");
 	alert("HELLO, " + name.toUpperCase() + "!");
 }
+
+function evenOrOdd() {
+	var num = prompt("Give me a number, please.");
+	if (num%2 === 0) {
+	 alert(num + " is an even number.");
+	}
+	else if (num%2 === 1) {
+		alert(num + " is an odd number.");
+	}
+	else {
+		alert("That is not a number")
+	}
+}
